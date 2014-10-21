b43ded9501f903a3d26571c7c711d9f01012ecc1
Anthony Cordani <Anthony@Anthonys-MacBook-Pro-Retina.local>
2014-10-21 10:17:00 +0200

    Procedure LISTER_SERVICE_GUICHET spec

---
+++ BDD/LISTER_SERVICE_GUICHET.sql
@@ -0,0 +1,4 @@
+######### PROCEDURE LISTER SERVICES PAR GUICHET #########
+## Return list of services for the ticket office
+## Parameters : 1 integers (ticket office id)
+LISTER_SERVICE_GUICHET(ID_GUICHET)
