e235b4ca6122c4ad662386811b143b12ac3141f2
Aurelien GASTON <nox@krondor.fr>
2014-11-09 07:13:04 +0100

    Add Link between Guichet and Service

---
+++ server/api/services/remove_guichet.php
@@ -0,0 +1,24 @@
+<?php
+
+    include($_SERVER['DOCUMENT_ROOT'].'/bootstrap/start.php');
+
+    try {
+        $params = Sanitize::get_params( array('id' => 0, 'id_guichet' => 0) );
+
+        // Fetch the guichet
+        $guichet = Guichet::Get($bdd, $params['id']);
+
+        // Fetch the service
+        $service = Service::Get($bdd, $params['id']);
+
+        $service->removeGuichet($bdd, $guichet);
+
+        http_response_code(200);
+        echo $service->to_json(); // Respond with the object
+
+    } catch (Exception $e) { // Something went wrong
+        http_response_code(500);
+        echo '{ error: true, errorMessage: "'.$e->getMessage().'" }';
+    }
+
+?>
